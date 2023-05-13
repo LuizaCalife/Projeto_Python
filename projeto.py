2e74f592219e0c3ae600a96824838d15b30f35bd
marialuisa.coimbra <marialuisacoimbra@gmail.com>
2023-05-12 22:53:41 -0300

    incio programa

--- projeto.py
+++ projeto.py
@@ -1,33 +1,8 @@
-dic = {}
-def adicionar(c,v): #lili:  c = categoria e v = valor 
-    dic[c] = []
-    dic[c].append(v)
-    return dic
+BANCO_DE_DADOS='transacoes.txt'
 
-def listar():
-    
-def atualizar():
-
-def deletar(c,v): # lili: c = categoria e v = valor
-    dic[c]= []
-    dic[c].remove(v)
-    return dic
-
-def filtrar_categoria():
-
-def extrato_categoria(c,v):# lili: c=categoria e v= valor
-    dic[c] = []
-    dic[c] = adicionar(sum(v)) # lili: natalia escolhe o dicionário/ categoria, que deseja retirar o extrato e aparece a soma do valor geral para ela.
-    return dic
-
-def funcionalidade_extra():
-
- #lili: precisa inicializar essa lista né??
-despesa =['alimentação', 'saúde', 'lazer', 'transporte', 'boletos']
-
-while True:
-    print('~Despesas de Natália~')
-    print('Menu de transações:')
+def menu():
+    print(f'{" ":^5}\033[1m~Despesas de Natália~\033[m')
+    print('\033[4mMenu de transações:\033[m')
     print('1. Adicionar transação')
     print('2. Listar transações')
     print('3. Atualizar transação')
@@ -36,27 +11,37 @@ while True:
     print('6. Extrato por categoria')
     print('7. Funcionalidade extra')
     print('0. Sair')
-    escolha=input('Selecione uma opção:')
+    escolha=int(input('Selecione uma opção:'))
+    return escolha
 
-    if escolha=='1':
-        valor = float(input('Digite o valor gasto:'))
-        print(adicionar(despesa,valor))
-    elif escolha=='2':
-        listar()
-    elif escolha=='3':
-        atualizar()
-    elif escolha=='4':
-        valor = float(input('Digite o valor gasto:'))
-        print(deletar(despesa,valor))
-    elif escolha=='5':
-        filtrar_categoria()
-    elif escolha=='6':
-        print(dic)
-        print(extrato_categoria(despesa,valor)) # lili: eu fiquei em duvida se ao passar o loop ele continua armazenando no dic
-    elif escolha=='7':
-        funcionalidade_extra()
-    elif escolha=='0':
+def adicionar(transacoes):
+    cont=len(transacoes) + 1
+    nome=input('Nome da transação:').lower().strip()
+    categoria=input('Categoria da transação:').lower().strip()
+    valor=float(input('Valor da transação:'))
+    transacoes[cont]={"nome": nome, "categoria": categoria, "valor": valor}
+    salvar(transacoes)
+    print('A transação foi adicionada com sucesso!')
+
+while True:
+    escolha=menu()
+    if escolha==1:
+        print('\033[4mCategorias:\033[m')
+        print('-Alimentação;\n-Saúde;\n-Lazer;\n-Transporte;\n-Boletos.\n')
+        adicionar(transacoes)
+    elif escolha==2:
+        listar(transacoes)
+    elif escolha==3:
+        atualizar(transacoes)
+    elif escolha==4:
+        deletar(transacoes)
+    elif escolha==5:
+        filtrar_categoria(transacoes)
+    elif escolha==6:
+        extrato(transacoes)
+    elif escolha==0:
+        print('Programa encerrado.')
         break
     else:
         print('Opção inválida.')
-        
+
